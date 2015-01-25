4f9ddd0bdca05c505a8ccbb8e22ac2df34f1b065
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-25 14:19:03 -0800

    Linux to windows time.

---
+++ Extensions/Date/DateTimeExts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class DateTimeExts
+    {
+        public static DateTime ToTimeFromUnix(this string text)
+        {
+            return DateTime.FromFileTime((long.Parse(text) + 11644473600) * 1000 * 1000 * 10);
+        }
+    }
+}
