947c93a51896b50c86b3127fee5e16dff567c294
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-25 20:44:13 -0800

    Escaping the path for the shell is a good start.

---
+++ Extensions/Strings/Escape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions.Strings
+{
+    public static class Escape
+    {
+        public static string PathForShell(this string path)
+        {
+            //Quotes are a good start
+            if(!path.StartsWith("\""))
+            {
+                path = "\"" + path;
+            }
+            if(!path.EndsWith("\""))
+            {
+                path = path + "\"";
+            }
+
+            return path
+                .Replace(@"\", @"\\")
+                .Replace("'", "\'");
+        }
+    }
+}
